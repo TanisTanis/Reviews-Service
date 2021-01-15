052a195edf111e559b32abfe5e9ce3bd1ece47d2
Tanis Kiel <tkiel12@gmail.com>
2021-01-15 10:08:15 -0800

    API 'GET' Call Working

--- server/index.js
+++ server/index.js
@@ -9,6 +9,17 @@ const db = require('../database');
 app.use(express.static('public'));
 app.use(express.json());
 
+app.get('/api/:id/reviews', (req, res) => {
+  const reviewId = req.params.id;
+  db.Review.find({ review_id: reviewId })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
