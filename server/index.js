06524372ec9f35b6f08736c3e3d15a5890c14323
Tanis Kiel <tkiel12@gmail.com>
2021-01-15 10:51:45 -0800

    Working POST request

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable object-shorthand */
 /* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
 const express = require('express');
@@ -14,6 +15,45 @@ app.get('/api/:id/reviews', (req, res) => {
   db.Review.find({ review_id: reviewId })
     .then((data) => {
       res.send(data);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
+app.post('/api/reviews', (req, res) => {
+  const {
+    user, location, reviewDate, title, body,
+  } = req.body;
+  const recommend = req.body.recommended;
+  let recommended;
+
+  if (recommend.toLowerCase() === 'true') {
+    recommended = true;
+  } else {
+    recommended = false;
+  }
+
+  db.Review.findOne({}).sort('-review_id')
+    .then((data) => {
+      const newId = data.review_id + 1;
+      db.Review.create({
+        review_id: newId,
+        user: user,
+        location: location,
+        review_date: reviewDate,
+        title: title,
+        body: body,
+        recommended: recommended,
+        ratings: {
+          overall: req.body.ratings.overall,
+          quality: req.body.ratings.quality,
+          durability: req.body.ratings.durability,
+        },
+      })
+        .then(() => {
+          res.send('Successful addition to database!');
+        });
     })
     .catch((err) => {
       res.send(err);
